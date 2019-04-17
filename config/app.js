e5bcaabfbfa0f32eea2ea8552a78f6368821a1b6
m3m3nto <m3m3nto@gmail.com>
2019-04-17 13:19:58 +0200

    Default http auth to false

--- config/app.js
+++ config/app.js
@@ -3,7 +3,7 @@ module.exports = {
   'database': 'mongodb://localhost:27017/giaa',
   'api_daily_quota': 200,
   'cids_dir': 'config/cids',
-  'basic_auth': true,
+  'basic_auth': false,
   'basic_auth_user': 'giaa',
   'basic_auth_pass': 'giaa'
 };
