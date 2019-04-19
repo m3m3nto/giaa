ec59c42df4ffa284d65140627211a8a601c5c0ba
m3m3nto <m3m3nto@gmail.com>
2019-04-19 10:35:36 +0200

    basic auth config was not managed

--- modules/auth.js
+++ modules/auth.js
@@ -2,11 +2,12 @@ let config = require('../config/app.js');
 let auth = require('basic-auth')
 
 module.exports = function(request, response, next) {
-  var user = auth(request);
+  if(!config.basic_auth) return next();
 
+  var user = auth(request);
   if (!user || config.basic_auth_user !== user.name || config.basic_auth_pass !== user.pass) {
     response.set('WWW-Authenticate', 'Basic realm="example"')
     return response.status(401).send()
   }
-  return next()
+  return next();
 }
