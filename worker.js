cd1bbe9f964ec1479e0dbf7a466f1a4926bbe851
m3m3nto <m3m3nto@gmail.com>
2020-10-07 14:37:54 +0200

    Trying to solve #10

--- worker.js
+++ worker.js
@@ -19,6 +19,13 @@ new CronJob('*/5 * * * * *', function() {
     }
     var remainingUrls = config.api_daily_quota - count;
 
+    function getLastPart(url) {
+      var parts = url.split("/");
+      return (url.lastIndexOf('/') !== url.length - 1
+         ? parts[parts.length - 1]
+         : parts[parts.length - 2]);
+    }
+
     if(remainingUrls > 0){
       var urls = Url.find({ 'status': 'pending' }).limit(remainingUrls);
       urls.exec(function (err, urls) {
@@ -26,44 +33,51 @@ new CronJob('*/5 * * * * *', function() {
         urls.forEach(function(url, index, arr){
           console.log('Notifying url: ' + url.location);
           var purl = parse(url.location, true);
-          account = Account.findOne({domain: purl.protocol + '//' + purl.hostname}).exec(function (err, account) {
-            var initializeIndexer = indexer.notify(url.location, url.type, account.cif);
-            initializeIndexer.then(function(urlDetails) {
+          try{
+            account = Account.findOne({ domain: {$regex: purl.hostname , $options: 'ig'} }).exec(function (err, account) {
+              if(account){
+                var initializeIndexer = indexer.notify(url.location, url.type, account.cif);
+                initializeIndexer.then(function(urlDetails) {
 
-              if(urlDetails.error){
-                url.response_status_code = urlDetails.error.code;
-                url.response_status_message = urlDetails.error.message;
-                url.notifytime = new Date();
-                url.status = 'error';
-                url.updatedat = new Date();
-              }else{
-                url.response_status_code = '200';
-                if(url.type == 'URL_DELETED'){
-                  url.response_status_message = urlDetails.urlNotificationMetadata.latestRemove.type;
-                  url.notifytime = urlDetails.urlNotificationMetadata.latestRemove.notifyTime;
-                }else{
-                  url.response_status_message = urlDetails.urlNotificationMetadata.latestUpdate.type;
-                  url.notifytime = urlDetails.urlNotificationMetadata.latestUpdate.notifyTime;
-                }
-                url.status = 'updated';
-                url.updatedat = new Date();
-              }
+                  if(urlDetails.error){
+                    url.response_status_code = urlDetails.error.code;
+                    url.response_status_message = urlDetails.error.message;
+                    url.notifytime = new Date();
+                    url.status = 'error';
+                    url.updatedat = new Date();
+                  }else{
+                    url.response_status_code = '200';
+                    if(url.type == 'URL_DELETED'){
+                      url.response_status_message = urlDetails.urlNotificationMetadata.latestRemove.type;
+                      url.notifytime = urlDetails.urlNotificationMetadata.latestRemove.notifyTime;
+                    }else{
+                      url.response_status_message = urlDetails.urlNotificationMetadata.latestUpdate.type;
+                      url.notifytime = urlDetails.urlNotificationMetadata.latestUpdate.notifyTime;
+                    }
+                    url.status = 'updated';
+                    url.updatedat = new Date();
+                  }
 
-              url.save(function (err) {
-                if (err) return handleError(err);
-                var client = io.connect( "http://localhost:3000");
-                client.once("connect", function () {
-                  client.emit('updatedUrl', url, function(){
-                    client.disconnect();
+                  url.save(function (err) {
+                    if (err) return handleError(err);
+                    var client = io.connect( "http://localhost:3000");
+                    client.once("connect", function () {
+                      client.emit('updatedUrl', url, function(){
+                        client.disconnect();
+                      });
+                    });
                   });
+
+                }, function(err) {
+                  console.log('GSC record not found');
                 });
-              });
-
-            }, function(err) {
-              console.log(err);
+              }else{
+                console.log(err)
+              }
             });
-          });
-
+          }catch(error){
+            console.log(error)
+          }
         });
       });
     }else{
